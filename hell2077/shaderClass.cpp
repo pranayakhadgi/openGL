7487511dc5806b5df6d2f6baf2095847abd09b11
Pranaya Khadgi Shahi <pranaya.khadgi99@gmail.com>
2026-03-03 19:15:52 -0600

    added Shader class to Header Files

--- hell2077/shaderClass.cpp
+++ hell2077/shaderClass.cpp
@@ -8,7 +8,6 @@
 #include <sstream>
 #include <iostream>
 
-#I need to import this class into the main source file (after midterm)
 class Shader
 {
 public:
@@ -26,3 +25,94 @@ public:
 	void setFloat(const std::string& name, float value)
 		const;
 };
+=======
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad> //incdlue galds to get all the required OpenGL headers
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	unsigned int ID; // our program ID
+
+	Shader(const char* vertexPath, const char* fragmentPath)
+	{
+		// 1. retrieve the vertex/fragment source code form filePath
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+
+		// ensure ifstream objects can throw exceptions:
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+		try
+		{
+			//open files
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+			std::stringstream vShaderstream, fShaderStream;
+
+			// read file's buffer contents into streams
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderFileStream << fShaderFile.rdbuf();
+
+			// close file handlers
+			vShaderFile.close();
+			fShaderFile.close();
+
+			// convert stream into string
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str()
+		}
+		catch (std::ifstream::failure e)
+		{
+			std::cout << "ERRPR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+		}
+		const char* vShaderCode = vertexCode.c_str();
+		const char* fShaderCode = fragmentCode.c_str();
+
+		// 2. compile the shaders
+		unsigned int vertex, fragment;
+		int success;
+		char infoLog[512];
+
+		// vertex Shader
+		vertex = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex, 1, &vShaderCode, NULL);
+		glCompileShader(vertex);
+
+		//print compile errors if any
+
+		glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLof(vertex, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+
+		//continue here... midterm is draining my soul
+
+
+	}
+	void use();
+
+
+	//the shader utility uniform function generalized!!
+	void setBool(const std::string &name, bool value)
+		const;
+	void setInt(const std::string& name, int value)
+		const;
+	void setFloat(const std::string& name, float value)
+		const;
+};
+
+#endif
+
